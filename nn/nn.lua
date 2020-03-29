34b3c501602bfc4474caa08c38477af2ed56bb24
BurningCole <alexander.e.cole@gmail.com>
2020-03-29 22:33:32 +0100

    add oppm config and make nn able to function as library

--- nn/nn.lua
+++ nn/nn.lua
@@ -3,23 +3,40 @@ local event=require("event")
 local serial=require("serialization")
 local m=c.modem
 
+local nn={}
+
 m.setStrength(2);
 m.open(1)
-local dictFile="dict.txt"
+local dictFile="/usr/etc/nn/dict.txt"
 
-local dict = {}
-local activeports = {}
+local args={...}
+local finished = (#args ~= 0)
 
-local function loadData(location)
+nn.dict = {}
+nn.activeports = {}
+
+local invDict={}
+
+local function doprint(text)
+  if(finished)
+    print(text)
+  end
+end
+
+local function nn.loadData(location)
   local file=io.open(location,"r")
-  if file == nil then return nil end
+  if file == nil 
+    return generateDict()
+  end
   local t = file:read("*all")
   t = serial.unserialize(t)
-  if t == nil then return nil end
+  if t == nil or #t==0 then 
+    return generateDict() 
+  end
   return t
 end
 
-local function saveData(data,location)
+local function nn.saveData(data,location)
   file=io.open(location,"w")
   file:write(serial.serialize(data))
   file:close()
@@ -38,10 +55,7 @@ local function invertDict(d)
   return invd
 end
 
-local posdict=loadData(dictFile)
-
-
-local function listenForNano()
+local function nn.listenForNano()
   resp=nil
   local loop=1
   while resp==nil and loop<5 do
@@ -54,52 +68,47 @@ local function listenForNano()
       if loop>10 then break end
       return respdata
     else
-      print("Conn error; retrying")
+      doprint("Conn error; retrying")
     end
   end
   return nil
 end
 
-local function send(command, ...)
+local function nn.send(command, ...)
   m.broadcast(1,"nanomachines",command,...)
   local resp = listenForNano()
   return resp
 end
 
-print("Getting nanomachine data")
-  resp=send("getTotalInputCount") or 1
-  local maxinput=resp[2]
-  print("\tMaximum Input: "..resp[2])
-  resp=send("getSafeActiveInputs")
-  local safeInputs=resp[2]
-  print("\tSafeInputs: ",resp[2])
-
-local function clearActive()
+local function nn.clearActive()
+  local resp="ClearingInputs:"
   for i,v in pairs(activeports) do
-    print("Input: ",i,v," off")
+    resp+="\nInput: "..i.." "..v.." off"
     send("setInput",v,false)
   end
   active={}
+  return resp
 end
 
-local function addEffect(code)
+local function findEffect(tmpDict,code)
     local resp = send("getActiveEffects")
     if resp[2]=="{}" or resp[2]=="" then
-      return
+      return tmpDict
     end
     resp=resp[2]:gsub("{","{\"")
     resp=resp:gsub(",","\",\"")
     resp=resp:gsub("}","\"}")
     resp=serial.unserialize(resp)
     for i=1,#resp,1 do
-      if(not dict[resp[i]]) then
-        dict[resp[i]]=code
-        print("Found: ",resp[i]," <= ",serial.serialize(code))
+      if(not tmpDict[resp[i]]) then
+        tmpDict[resp[i]]=code
+        doprint("Found: "..resp[i].." <= "..serial.serialize(code))
       end
     end
+    return tmpDict
 end
 
-local function genDictRecurse(limit,ports,level)
+local function genDictRecurse(tmpDict,limit,ports,level)
   local tbl={}
   for k,v in pairs(ports) do
     tbl[k+1]=val
@@ -107,65 +116,63 @@ local function genDictRecurse(limit,ports,level)
   for j=0,limit,1 do
     if(#ports~=0)then
     if(0==j) then
-      print("Checking ",serial.serialise(ports),"..")
-      addEffect(ports)
+      doprint("Checking "..serial.serialise(ports).."..")
+      tmpDict=findEffect(tmpDict,ports)
     else
       tbl[1]=j
-      print("Checking ",serial.serialise(tbl),"..")
+      doprint("Checking "..serial.serialise(tbl).."..")
       send("setInput",j,true)
-      addEffect(tbl)
+      tmpDict=findEffect(tmpDict,tbl)
       if(level < safeInputs) then
-        genDictRecurse(j-1,tbl,level+1)
+        tmpDict=genDictRecurse(tmpDict,j-1,tbl,level+1)
       end
       send("setInput",j,false)     
     end
     end
   end
+  return tmpDict
 end
     
-
-local function generateDict()
-  print("Clearing inputs")
-  clearActive()
-  print("\nChecking combinations")
-  genDictRecurse(maxinput,{},1)
-  savedata(dict,dictfile)
+local function nn.generateDict()
+  doprint(nn.clearActive())
+  doprint("\nChecking combinations:")
+  local tmpDict={}
+  tmpDict=genDictRecurse(tmpDict,maxinput,{},1)
+  savedata(tmpDict,dictfile)
+  return tmpDict
 end
 
-
-print("\nGetting active ports")
-for i=1,maxinput,1 do
-  resp=send("getInput",i)
-  if(resp[3]) then
-    activeports[#activeports+1]=i
-    print("Port: ",i," on")
-  else if(i%5==0) then
-    print(i," Scanned")
-  end end
-end
-
-print("\nPreparing dictionary")
-if posdict~=generateDict() then dict = posdict end
-
-local invDict=invertDict(dict)
-
-local function handlePlayerInfo()
-  print("Player Information")
+local function nn.handlePlayerInfo()
+  local pinfo={}
+  
   resp=send("getName")
   if resp[2]=="" then resp[2]="Unknown" end
-  print(resp[2]..":")
+  pinfo.name=resp[2]
   resp=send("getAge")
-  print("  Age:      "..resp[2])
+  pinfo.age=resp[2]
   resp=send("getHealth")
-  print("  Health:   "..resp[2])
+  pinfo.health=resp[2]
   resp=send("getHunger")
-  print("  Hunger:   "..resp[2])
-  print("  Saturation: "..resp[3])
+  pinfo.hunger=resp[2]
+  pinfo.saturation=resp[3]
   resp=send("getExperience")
-  print("  Exp:      "..resp[2].."\n")
+  pinfo.xp=resp[2]
+  
+  setmetatable(pinfo,{
+    __tostring=function(pinfo)
+      return "Player Information"..
+      pinfo.name..":"..
+      "\n  Age:      ".. pinfo.age..
+      "\n  Health:   "..pinfo.health..
+      "\n  Hunger:   "..pinfo.hunger..
+      "\n  Saturation: "..pinfo.saturation..
+      "\n  Exp:      "..pinfo.xp.."\n"
+    end
+  })
+  return pinfo
 end
 
-local function switchInput(comnum)
+local function nn.switchInput(comnum)
   local dictValue=dict[comnum]
   if dictValue ~= nil then
     comnum=dictValue
@@ -178,127 +185,134 @@ local function switchInput(comnum)
       i="unknown"
     end
   end
-  local toAdd=comnum
-  local toRemove=activeports
-  for k,v in pairs(toAdd) do
-    for p,o in pairs(toRemove) do
+  for
+  local changes={added=comnum,removed=activeports}
+  for k,v in pairs(changes.added) do
+    for p,o in pairs(changes.removed) do
       if(v==o) then
-        table.remove(toAdd,k)
-        table.remove(toRemove,p)
+        table.remove(changes.added,k)
+        table.remove(changes.removed,p)
       end
     end 
   end
-  if(#toAdd==0) then
-    print("Already active")
-  end
-  if(#toAdd+#activeports>safeInputs) then
-    for k,v in pairs(toRemove) do
-      print("Input: ",v," off")
+  if(#changes.added+#activeports>nn.safeInputs) then
+    for k,v in pairs(changes.remove) do
       send("setInput",v,false)
       for p,o in pairs(activeports) do
         if(o==v) then table.remove(activeports,p) end
       end
     end
+  else
+    changes.removed={}
   end
-  for k,v in pairs(toAdd) do
-    print("Input: ",v," on")
+  for k,v in pairs(changes.added) do
     send("setInput",v,true)
     table.insert(activeports,v) 
   end
-  local com=""
-  if resp then
-    com="off"
-  else
-    com="on"
-  end
-  print("Input: ",comnum," ",com,"\n")
+  return setmetatable(changes,{__tostring=function(changes)
+    local resstring="Changes:"
+    for _,v in pairs(changes.removed) do
+      resstring+="\n Input: "..v.." Off"
+    end
+    for _,v in pairs(changes.added) do
+      resstring+="\n Input: "..v.." On"
+    end
+    return resstring
+  end})
 end
 
-local function listActiveEffects()
-  print("Active effects:")
+local function nn.listActiveEffects()
   local resp = send("getActiveEffects")
-  if resp[2]=="{}" or resp[2]=="" then
-    print("none")
-    return false
+  if(resp==nil) then
+    return ""
   end
-  resp=resp[2]:gsub("([^{,}]+)","\"%1\"")
-  resp=serial.unserialize(resp)
-  for i=1,#resp,1 do
-    print(i .. ": " .. resp[i] .. "\n")
-  end
-  return false
-end
-
-local function checkInput(comnum)
-  local resp=send("getInput",comnum)
-  resp = resp[3]
-  local com=""
-  if resp then
-    com="off"
+  if resp[2]=="" or resp[2]=="{}" then
+    resp=nil
   else
-    com="on"
+    resp=resp[2]:gsub("([^{,}]+)","\"%1\"")
+    resp=serial.unserialize(resp)
   end
-  print("Input: ",comnum," ",com,"\n")
+  local effects=setmetatable(resp,{
+    __tostring=function(table)
+      local tabledata=""
+      for i=1,#resp do
+        tabledata=tabledata.."\n"..i .. ": " .. resp[i])
+      end
+      if(#resp==0){
+        tabledata="\nnone"
+      }
+      return "Active effects:"..tabledata
+    end
+  })
+  return false
 end
 
-local function handleNanoInfo()
-  print("Nanobot information:")
+local function nn.handleNanoInfo()
+  local nanoInfo={}
   local resp=send("getPowerState")
-  print("\tEnergy: ",resp[2].."/"..resp[3])
+  nanoInfo.energy=resp[2]
   resp=send("getActiveEffects")
-  local _,count = string.gsub(resp[2],",","")
-  if resp[2]~="{}" then
-    count=count+1 --for first value
-  end
-  print("\tEffects: "..count)
-  print("\tSafeInputs: ",#activeports.."/"..safeInputs)
-  print("\tMaximum Input: "..maxinput)
-  return false
+  resp = resp[2]:gsub("([^{,}]+)","\"%1\"")
+  nanobot.effects=serial.unserialize(resp)
+  
+  setmetatable(nanoInfo,{
+    __tostring=function(nanobot)
+      return "Nanobot information:"..
+      "\n Energy: "..nanobot.energy.."/"..nanobot.maxenergy..
+      "\n Effects: "..#(nanobot.effects)..
+      "\n Safe Inputs: "..#(nn.activeports).."/"..nanobot.safeInputs..
+      "\n Maximum Input: "..nn.maxinput
+    end
+  })
+  return effects
 end
 
-function listEffects()
-  print("All effects:")
-  local index=1
-  for k,i in pairs(dict) do
-    i=serial.serialize(i)
-    i=i:gsub("[{}]","")
-    io.write(k..": "..i.." \t")
-    if index % 4 == 0 then
-      io.write("\n")
+local function nn.listEffects()
+  local effects=dict
+  setmetatable(effects,{
+    __tostring=function(effects)
+      local effectsString=""
+      for i,v in pairs(effects) do
+       i=serial.serialize(i)
+        i=i:gsub("[{}]","")
+        effectsString+="\n"..k..": "..i
+      end
+      return "All effects:"..effectsString
     end
-    index=index+1
-  end
-  io.write("\n")
-  return false
+  })
+  return effects
 end
 
-local function addEffect()
-  print("Add effect:")
-  local input=nil
-  while input==nil do
-    io.write("Insert Effect Input: ")
-    input = io.read()
-    if input == "q" then return false end
-    local exclusion =input:find("[^%n,]")
-    if(dict[input]) then 
-      input=dict[input] 
-    elseif exclusion ~= nil then 
-      print("Effect Input must be number, existing element or number list (q to quit)")
-    else
-      input="{"..input.."}"
-      input=serial.unserialize(input)
+local function nn.addEffect(input,Effect)
+  if(input==nil) then
+    doprint("Add effect:")
+    local input=nil
+    while input==nil do
+      io.write("Insert Effect Input: ")
+      input = io.read()
+      if input == "q" then return false end
+      local exclusion =input:find("[^%n,]")
+      if(dict[input]) then 
+        input=dict[input] 
+      elseif exclusion ~= nil then 
+        doprint("Effect Input must be number, existing element or number list (q to quit)")
+      else
+        input="{"..input.."}"
+        input=serial.unserialize(input)
+      end
+      
     end
-    
   end
-  local Effect = ""
-  while #Effect < 2 do
-    io.write("Insert Effect Name: ")
-    Effect = io.read()
-    if Effect == "q" then return false end
-    if #Effect < 2 then print("Effect Name must be longer than 2 characters (q to quit)") end
-    if dict[Effect]~= nil then
-      print("Effect name already used (q to quit)")
-      Effect = ""
+  if(Effect == nil) then
+    while #Effect < 2 do
+      io.write("Insert Effect Name: ")
+      Effect = io.read()
+      if Effect == "q" then return false end
+      if #Effect < 2 then doprint("Effect Name must be longer than 2 characters (q to quit)") end
+      if dict[Effect]~= nil then
+        doprint("Effect name already used (q to quit)")
+        Effect = ""
+      end
     end
   end
   dict[Effect]=input
@@ -307,16 +321,17 @@ local function addEffect()
   return false
 end
 
-local function removeEffect()
-  print("Remove effect:")
-  local Effect = nil
-  while Effect == nil do
-    io.write("Insert Effect Name: ")
-    Effect = io.read()
-    if Effect == "q" then return nil end
-    if dict[Effect]== nil then
-      print("Effect name not used (q to quit)")
-      Effect = nil
+local function nn.removeEffect(Effect)
+  if(Effect==nil) then
+    doprint("Remove effect:")
+    while Effect == nil do
+      io.write("Insert Effect Name: ")
+      Effect = io.read()
+      if Effect == "q" then return nil end
+      if dict[Effect]== nil then
+        doprint("Effect name not used (q to quit)")
+        Effect = nil
+      end
     end
   end
   dict[Effect]=nil
@@ -326,37 +341,65 @@ local function removeEffect()
 end
 
 local function commands()
-  print("Nanomachine Help menu:")
-  print("Standard Commands:")
-  print(" p | player: Get data about player")
-  print(" q | quit:   Stop program")
-  print(" l | list:   Get list of saved effects and Inputs")
-  print(" a | active: Get active effects")
-  print(" n | nano:   Get data about nanomachines")
-  print(" + | add:    Link effect to Input")
-  print(" - | remove: Remove effect link")
-  print(" h | help:   Show this menu")
-  print("\nInputs:")
-  print(" 1-Max_input: enables/disables Input number given")
-  print(" effect_name: enables/disables Input linked to effect")
-  return false
+  return "Nanomachine Help menu:"..
+  "\nStandard Commands:"..
+  "\n p | player: Get data about player"..
+  "\n q | quit:   Stop program"..
+  "\n l | list:   Get list of saved effects and Inputs"..
+  "\n a | active: Get active effects"..
+  "\n n | nano:   Get data about nanomachines"..
+  "\n c | clear: remove all effects"..
+  "\n + | add:    Link effect to Input"..
+  "\n - | remove: Remove effect link"..
+  "\n h | help:   Show this menu"..
+  "\n\nInputs:"..
+  "\n 1-Max_input: enables Input number given"..
+  "\n effect_name: enables Input linked to effect"
 end
 
-local q() do 
-  print("quit") 
-  return true
+local function nn.init(port,dict)
+
+  doprint("Getting nanomachine data")
+  resp=send("getTotalInputCount") or 1
+  nn.maxinput=resp[2]
+  doprint("\tMaximum Input: "..resp[2])
+  resp=send("getSafeActiveInputs")
+  nn.safeInputs=resp[2]
+  doprint("\tSafeInputs: "..resp[2])
+  resp=send("getPowerState")
+  nn.maxenergy=resp[3]
+  doprint("\Max Energy: "..resp[3])
+
+  doprint("\nGetting active ports")
+  for i=1,maxinput,1 do
+    resp=send("getInput"..i)
+    if(resp[3]) then
+      activeports[#activeports+1]=i
+      doprint("Port: "..i.." on")
+    elseif(i%5==0) then
+      doprint(i.." Scanned")
+    end
+  end
+  
+  doprint("\nPreparing dictionary")
+  dict=loadData(dictFile)
+  invDict=invertDict(dict)
+end
+
+local function q()
+  return "quitting"
 end
 
 local funcList = setmetatable({
-  p = handlePlayerInfo, player = handlePlayerInfo,
-  q = q, quit = q,
-  l = listEffects, list = listEffects,
-  a = listActiveEffects, active = listActiveEffects,
-  n = handleNanoInfo, nano = handleNanoInfo
-  + = addEffect, add = addEffect,
-  - = removeEffect, remove = removeEffect,
+  p = nn.handlePlayerInfo, player = nn.handlePlayerInfo,
+  ["q"] = q, quit = q,
+  l = nn.listEffects, list = nn.listEffects,
+  a = nn.listActiveEffects, active = nn.listActiveEffects,
+  n = nn.handleNanoInfo, nano = nn.handleNanoInfo,
+  ["+"] = nn.addEffect, add = nn.addEffect,
+  ["-"] = nn.removeEffect, ["remove"] = nn.removeEffect,
   h = commands, help = commands,
-  c = clearActive, clear = clearActive, none = clearActive
+  c = nn.clearActive, clear = nn.clearActive, none = nn.clearActive
 },
 {
   __index = function(_,index)
@@ -377,12 +420,23 @@ local function readNext()
   local command=io.read()
   local func=funcList[command]
   if func ~= nil then
-    return func()
+    local resp = func()
+    print(resp)
+    return (func==q)
   end
   return false
 end
 
-local finished = false
+if(finished==true) then
+  if(args[1]~="req") then
+    nn.init()
+    for i=1,#args do
+      print((funcList[args[i]])())
+    end
+  end
+  return nn
+end
+nn.init()
 while not finished do
   finished=readNext()
 end
